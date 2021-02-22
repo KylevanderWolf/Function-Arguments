d84c24dfd30b59d17c544e52c751a7c52d2bd346
kyle van der Wolf <kylevanderwolf@gmail.com>
2021-02-22 12:46:49 +0100

    Function Arguments

---
+++ arguments.js
@@ -0,0 +1,11 @@
+
+
+//Multiple Arguments
+const wallPaint = (color1, color2) => {
+    console.log("The north wall has been painted " + color1)
+    console.log("The south-east wall has been painted " + color2)
+}
+
+//Calling the Function
+wallPaint('red', 'green')
+
